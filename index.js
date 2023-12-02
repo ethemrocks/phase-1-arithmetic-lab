63a6fe9f9333693cfa2b006b72226cfb542beb71
Ethem Kacic <ethem3k@gmail.com>
2023-12-02 14:37:12 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+// Assume you have the following variables defined somewhere in your code
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+
+const num3 = 22;
+const num4 = 6; // Updated to result in a remainder of 4 when num3 is divided by num4
+const mod = num3 % num4;
+
+const numbersArray = [2, 4, 6, 8, 10, 20];
+const max = Math.max(...numbersArray);
+
+// Export variables if needed (assuming you are using CommonJS modules)
+module.exports = {
+  num1,
+  num2,
+  multiply,
+  random,
+  num3,
+  num4,
+  mod,
+  max,
+};
